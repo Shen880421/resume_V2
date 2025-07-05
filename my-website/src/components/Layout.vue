d2b00c0e05da007de4a07b693275962c80579d92
ashen <solove5171@gmail.com>
2025-07-05 22:20:15 +0800

    feat: 更新 App.vue，新增 Layout 元件並調整樣式

---
+++ my-website/src/components/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full h-screen flex">
+        <div class="w-1/6 bg-slate-600 text-cyan-50">
+            左邊
+        </div>
+        <div class="w-5/6">
+            右邊
+        </div>
+    </div>
+</template>
+<script setup></script>
+<style scoped></style>
