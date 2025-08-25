6ca66471c044b56080e02b9984a41aeadaa84003
Shen <solove5171@gmail.com>
2025-08-25 21:10:15 +0800

    feat: 更新出缺勤平台及腦動時光的測試帳號資訊，並新增展示影片連結

--- my-website/src/components/Portfolio.vue
+++ my-website/src/components/Portfolio.vue
@@ -1,65 +1,89 @@
 <template>
-  <h1 class="flex justify-center text-4xl font-bold mt-36 mb-12">Portfolio</h1>
-  <section class="grid max-sm:grid-cols-1 sm:grid-cols-3 gap-20 h-full m-20">
-    <div class="flex justify-center">
-      <a href="https://redux-toolkits-five.vercel.app/" class="">
-        <img
-          class=""
-          src="../assets/wannabakerylogo.jpg"
-          alt="redux_toolkit_website"
-        />
-        <p class="text-amber-950 bg-indigo-100">Wannabakery-Redux Toolkit</p>
-      </a>
-    </div>
-    <div class="flex justify-center">
-      <a href="http://attendfs101.ddns.net/" class="">
-        <img
-          class=""
-          src="../assets/dashboardimg.png"
-          alt="attendance_website"
-        />
-        <p class="text-amber-950 bg-indigo-100">出缺勤平台</p>
-        <p class="text-amber-950 bg-indigo-100">
-          管理端測試帳號:admin@demo.com/test1234
-        </p>
-        <p class="text-amber-950 bg-indigo-100">
-          企業端測試帳號:adv-user@test.com/test1234
-        </p>
-      </a>
-    </div>
-    <div class="flex justify-center">
-      <a
-        href="https://github.com/TobyWuNumOne/tutor-matching-platform.git"
-        class=""
-      >
-        <img
-          class=""
-          src="../assets/tutorLogo.png"
-          alt="tutor-matching-platform"
-        />
-        <p class="text-amber-950 bg-indigo-100">家教媒合平台</p>
-      </a>
-    </div>
-    <div class="flex justify-center">
-      <a href="https://github.com/Shen880421/attendence_system" class="">
-        <img class="" src="../assets/checkinoutLogo.png" alt="Spring boot" />
-        <p class="text-amber-950 bg-indigo-100">Spring boot 打卡系統</p>
-      </a>
-    </div>
-    <div class="flex justify-center">
-      <a href="https://bt.pair.tw/" class="">
-        <img
-          class=""
-          src="../assets/braintimeLogo.png"
-          alt="腦動時光 BrainTime"
-        />
-        <p class="text-amber-950 bg-indigo-100">腦動時光 BrainTime</p>
-        <p class="text-amber-950 bg-indigo-100">測試醫院:臺大醫院</p>
-        <p class="text-amber-950 bg-indigo-100">測試醫生:doctortest/password</p>
-        <p class="text-amber-950 bg-indigo-100">測試病人:A153340409/password</p>
-      </a>
-    </div>
-  </section>
+    <h1 class="flex justify-center text-4xl font-bold mt-36 mb-12">
+        Portfolio
+    </h1>
+    <section class="grid max-sm:grid-cols-1 sm:grid-cols-3 gap-20 h-full m-20">
+        <div class="flex justify-center">
+            <a href="https://redux-toolkits-five.vercel.app/" class="">
+                <img
+                    class=""
+                    src="../assets/wannabakerylogo.jpg"
+                    alt="redux_toolkit_website"
+                />
+                <p class="text-amber-950 bg-indigo-100">
+                    Wannabakery-Redux Toolkit
+                </p>
+            </a>
+        </div>
+        <div class="flex justify-center">
+            <a href="http://attendfs101.ddns.net/" class="">
+                <img
+                    class=""
+                    src="../assets/dashboardimg.png"
+                    alt="attendance_website"
+                />
+                <p class="text-amber-950 bg-indigo-100">出缺勤平台</p>
+                <p class="text-amber-950 bg-indigo-100">
+                    管理端測試帳號:admin@demo.com/test1234
+                </p>
+                <p class="text-amber-950 bg-indigo-100">
+                    企業端測試帳號:adv-user@test.com/test1234
+                </p>
+            </a>
+        </div>
+        <div class="flex justify-center">
+            <a
+                href="https://github.com/TobyWuNumOne/tutor-matching-platform.git"
+                class=""
+            >
+                <img
+                    class=""
+                    src="../assets/tutorLogo.png"
+                    alt="tutor-matching-platform"
+                />
+                <p class="text-amber-950 bg-indigo-100">家教媒合平台</p>
+            </a>
+        </div>
+        <div class="flex justify-center flex-col">
+            <a href="https://github.com/Shen880421/attendence_system" class="">
+                <img
+                    class=""
+                    src="../assets/checkinoutLogo.png"
+                    alt="Spring boot"
+                />
+                <p class="text-amber-950 bg-indigo-100">Spring boot 打卡系統</p>
+            </a>
+            <p
+                class="text-amber-950 bg-indigo-100 text-xl text-center hover:bg-indigo-300"
+            >
+                <a href="https://youtu.be/kW8lJJUqa84">看展示影片</a>
+            </p>
+        </div>
+        <div class="flex justify-center flex-col">
+            <a href="https://bt.pair.tw/" class="">
+                <img
+                    class=""
+                    src="../assets/braintimeLogo.png"
+                    alt="腦動時光 BrainTime"
+                />
+                <p class="text-amber-950 bg-indigo-100">腦動時光 BrainTime</p>
+                <p class="text-amber-950 bg-indigo-100">測試醫院:臺大醫院</p>
+                <p class="text-amber-950 bg-indigo-100">
+                    測試醫生:doctortest/password
+                </p>
+                <p class="text-amber-950 bg-indigo-100">
+                    測試病人:A153340409/password
+                </p>
+            </a>
+            <p
+                class="text-amber-950 bg-indigo-100 text-xl text-center hover:bg-indigo-300"
+            >
+                <a href="https://youtu.be/6DiD7r_aDoY?si=zvVuIfZcQzCwzwLR"
+                    >看展示影片</a
+                >
+            </p>
+        </div>
+    </section>
 </template>
 <script setup></script>
 <style scoped></style>
